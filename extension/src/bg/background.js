b40089aef291ebb18e587d806e0abba4b09c40ff
Raphie Palefsky-Smith <me@raphieps.com>
2015-06-20 19:44:13 -0700

    TABS TABS TABS

--- extension/src/bg/background.js
+++ extension/src/bg/background.js
@@ -7,3 +7,55 @@ chrome.runtime.onMessageExternal.addListener(function(request, sender, sendRespo
     });
   });
 });
+
+// var responseListener = function(details){
+//     var rule = {
+//         "name": "Origin",
+//         "value": "http://localhost:3000"
+//     };
+//     console.log(details.responseHeaders);
+//     details.responseHeaders.push(rule);
+//     return {responseHeaders: details.responseHeaders};
+// };
+//
+//  chrome.webRequest.onHeadersReceived.addListener(responseListener,
+//      {urls: [   "*://*/*" ] },
+//      ["blocking", "responseHeaders"]);
+
+
+var minimized_id = null;
+var urlToId = {};
+var idToUrl = {};
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+
+  if (request.preLoad) {
+    var doIt = function() {
+      chrome.tabs.create({windowId: minimized_id, url: request.preLoad}, function(tab) {
+        urlToId[request.preLoad] = tab.id;
+        idToUrl[tab.id] = request.preLoad;
+        sendResponse({startLoad: request.preLoad});
+      });
+    }
+    if (!minimized_id) {
+      chrome.windows.create(function(minimized) {
+        chrome.windows.update(minimized.id, {state: "minimized"}, function() {
+          minimized_id = minimized.id;
+          doIt();
+        });
+      });
+    } else {
+      doIt();
+    }
+    return;
+  }
+
+  if (request.load) {
+    var tab_id = urlToId[request.load];
+    chrome.tabs.move(tab_id, {windowId: sender.tab.windowId, index: sender.tab.index}, function() {
+      // console.log(arguments);
+      chrome.tabs.update(tab_id, {highlighted: true});
+      chrome.tabs.remove(sender.tab.id);
+    });
+  }
+});
--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -1,33 +1,7 @@
-// var API_URL = "https://a16z.herokuapp.com";
-var API_URL = "https://16a664b2.ngrok.com";
-
-var cacheUrl = function(url) {
-	var frame = document.createElement("iframe");
-	frame.style.display = "none";
-	frame.src = API_URL + "/frame/" + encodeURIComponent(url);
-	document.body.appendChild(frame);
-};
-
-if (location.href.indexOf(API_URL) !== -1) {
-	// we're on our own frame site, don't inject or else we get a loop!
-} else {
-	// we're on something like nytimes.com, yay, time to inject
-	chrome.runtime.onMessage.addListener(function(request) {
-		alert(JSON.stringify(request));
-	});
-	cacheUrl(location.href);
+window.preLoad = function(url) {
+	chrome.runtime.sendMessage({preLoad: url});
 }
 
-// This needs to run on the iframe page
-/*
-var updateCacheStatus = function(e) {
-    chrome.runtime.sendMessage("kdikmknnallalfkidkhnjleliajhople", {
-        cacheEvent: e,
-        url: decodeURIComponent(location.href.match(/.*\/(.*)$/)[1])
-    });
+window.load = function(url) {
+	chrome.runtime.sendMessage({load: url});
 }
-
-window.applicationCache.addEventListener("progress", function (e) {
-    updateCacheStatus({total: e.total, loaded: e.loaded});
-});
-*/
