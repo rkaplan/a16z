88d6640d8b25e4f3b7e78e00c6834b001fe97d83
Raphie Palefsky-Smith <me@raphieps.com>
2015-06-20 17:10:56 -0700

    Add caching and cache monitoring logic

--- extension/src/bg/background.js
+++ extension/src/bg/background.js
@@ -1,13 +1,9 @@
-// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    tabs.forEach(function(tab) {
+      chrome.tabs.sendMessage(tab.id, request, function(response) {
+        console.log(response.farewell);
+      });
+    });
   });
+});
--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -1,13 +1,33 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+// var API_URL = "https://a16z.herokuapp.com";
+var API_URL = "https://16a664b2.ngrok.com";
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+var cacheUrl = function(url) {
+	var frame = document.createElement("iframe");
+	frame.style.display = "none";
+	frame.src = API_URL + "/frame/" + encodeURIComponent(url);
+	document.body.appendChild(frame);
+};
 
-	}
-	}, 10);
+if (location.href.indexOf(API_URL) !== -1) {
+	// we're on our own frame site, don't inject or else we get a loop!
+} else {
+	// we're on something like nytimes.com, yay, time to inject
+	chrome.runtime.onMessage.addListener(function(request) {
+		alert(JSON.stringify(request));
+	});
+	cacheUrl(location.href);
+}
+
+// This needs to run on the iframe page
+/*
+var updateCacheStatus = function(e) {
+    chrome.runtime.sendMessage("kdikmknnallalfkidkhnjleliajhople", {
+        cacheEvent: e,
+        url: decodeURIComponent(location.href.match(/.*\/(.*)$/)[1])
+    });
+}
+
+window.applicationCache.addEventListener("progress", function (e) {
+    updateCacheStatus({total: e.total, loaded: e.loaded});
 });
+*/
